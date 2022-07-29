6da6ad0e9ba1f8b84ae2b1d2943e58c0440a0fe0
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2022-07-29 18:18:09 +0530

    feat: 05_sumAll complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, lastNumber) {
+    let numberArray = [];
+    if ((Math.sign(lastNumber) === -1) || (Math.sign(firstNumber) === -1)){
+        return "ERROR";
+    } else if ((typeof lastNumber !== 'number') || (typeof firstNumber !== 'number')) {
+        return "ERROR";
+    };
+    if (lastNumber > firstNumber){
+        arrayZeus(firstNumber, lastNumber, numberArray);
+    } else {
+        arrayZeus(lastNumber, firstNumber, numberArray);
+    }
+    let finalSum = numberArray.reduce((sum, x) => sum + x, 0);
+    return finalSum;
 };
+
+function arrayZeus(smallNumber, bigNumber, numberArray) {
+    for (let number = smallNumber; number <= bigNumber; number++) {
+        numberArray.push(number);
+    }
+    return numberArray;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
