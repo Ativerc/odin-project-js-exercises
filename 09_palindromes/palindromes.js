dad976718e328da0a965043a32ac59f7834a5c9d
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2022-07-31 13:08:20 +0530

    feat: 09_palindromes complete!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    filteredString = inputString.toLowerCase().replace(/[^a-z]/g, "");
+    fStringArray = filteredString.split("");
+    reversedString = fStringArray.reverse().join("");
+    console.log(`reversedString: ${reversedString}`)
+    return (reversedString == filteredString);
 };
 
 // Do not edit below this line
