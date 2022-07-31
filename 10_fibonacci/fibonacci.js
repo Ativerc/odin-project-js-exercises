66e458af3f8155ab2fbdf9ab8a81d26a76a2d27d
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2022-07-31 15:05:38 +0530

    feat: 10_fibonacci complete!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
-
+const fibonacci = function(specific) {
+    if (Math.sign(specific) == -1){
+        return "OOPS";
+    }
+    fibArray = realFibonacci(1, 1, specific);
+    // console.log(fibArray[specific-1]);
+    return fibArray[specific-1];
 };
 
+
+function realFibonacci(seed0, seed1, iter){    
+    let fibArr = [];    
+    fibArr.push(seed0, seed1);    
+    for (let i = 0; i <= iter; i++){        
+        let next = seed0 + seed1;        
+        // console.log(`${i}: seed0; ${seed0} seed1: ${seed1} next:${next}`);        
+        seed0 = seed1;        
+        seed1 = next;        
+        fibArr.push(next);
+    }    
+        // console.log(fibArr);
+        return fibArr;
+}
+
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports =  (fibonacci);
