c74af92eb3f086c4e576e95e7e36ebeaf50386c2
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2022-07-31 11:44:16 +0530

    feat: 07_tempConversion complete!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  cel = ((far - 32)*5)/9;
+  return Math.round(cel*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  far = 1.8*cel + 32;
+  return Math.round(far*10)/10;
 };
 
 // Do not edit below this line
