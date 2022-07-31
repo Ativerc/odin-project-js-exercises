6c436818074136eb229ae2ed5732a24544faf34f
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2022-07-31 22:18:52 +0530

    feat: 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    const d = new Date();
+    let year = d.getFullYear();
+    let maxage = 0;
+    let maxObject = {};
+    for (let key of peopleArray) {
+        let age;
+        if (key.yearOfDeath == undefined){
+            age = year - key.yearOfBirth;
+        } else {
+            age = key.yearOfDeath - key.yearOfBirth;
+        }
+        if (age > maxage ) {
+            maxage = age;
+            maxObject = key;
+        }
+    }
+    return(maxObject);
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
